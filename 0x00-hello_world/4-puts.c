48ec156daae11c269909f436a8dcf55511e7b0fe
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-05 19:20:28 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Aprogram that prints a line function
+ * main - A program that prints a line function
  * Return: 0 (success)
- * */
+ */
 int main(void)
 {
 puts ("\"Programming is like building a multilingual puzzle");
