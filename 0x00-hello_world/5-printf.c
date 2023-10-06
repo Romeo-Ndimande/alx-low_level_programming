1d2221ad23f3222204cae5c6b7f48b5bd93dd50f
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-05 19:31:45 -0700

    0-preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+i#include <stdio.h>
+/**
+ * main - A program that prints a line using printf function
+ * Return: 0 (sucess)
+ */
+init main(void)
+{
+	printf("with proper grammar, the outcome is a piece of art,\n")
+		return (0);
+}
