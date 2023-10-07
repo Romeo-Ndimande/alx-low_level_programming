17b5defed1a6ca4733136de62aa7d74a192e496c
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-06 19:22:53 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: always 0
+ */
+int mode(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			pitchar(i);
+	putchar("\n");
+	return (o)
+}
