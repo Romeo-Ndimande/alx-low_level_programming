6945d5790858e7ab35cb7cd4ec02a2db1632bf14
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-09 06:34:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,18 @@ int main(void)
 {
 	int d, p;
 
-		for (d = '0'; d < '9'; d++);
+	for (d = '0'; d < '9'; d++)
 	{
-		for (p = d + 1; p <= '9'; p++);
+	for (p = d + 1; p <= '9'; p++)
 	{
-		if (p != d);
+	if (p != d)
 	{
-		putchar(d);
-		putchar(p);
-		if (d == '8' && p == '9');
-		continue;
-		putchar(',');
-		putchar(' ');
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9');
+	continue;
+	putchar(',');
+	putchar(' ');
 	}
 	}
 	}
