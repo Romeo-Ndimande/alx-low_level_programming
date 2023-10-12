4cabf14cc47d16aaaf57807b8ec8d4e90c044e3e
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-11 20:41:17 -0700

    functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,8 @@
 
 /**
  * _isalpha - checks alphabetic character
- * @C: the character will be checked
+ * @c: the character will be checked
+ *
  * Return: 1 if c  is a letter, otherwise 0
  */
 int _isalpha(int c)
