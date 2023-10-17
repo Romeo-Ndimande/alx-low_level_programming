3abca645c4c911c4249a39c92ca1ac1d6beb5ad9
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-16 18:41:39 -0700

    rev-string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,10 +13,10 @@ void rev_string(char *s)
 
 	while (s[counter] != '\0')
 		counter++;
-	for (i <0; i < counter; i++)
+	for (i < 0; i < counter; i++)
 	{
 		counter--;
-		rev = [i];
+		rev = s[i];
 		s[i] = s[counter];
 		s[counter] = rev;
 	}
