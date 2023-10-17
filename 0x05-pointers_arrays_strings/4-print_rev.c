c1cf3d51b8db07c29884647432470abb9d47699f
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-16 18:23:05 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - funtion prints a string reverse then a new line
+ * @s: string
+ * Retunr:0
+ */
+
+void print_rev(char *s)
+{
+	int longi =0;
+	int o;
+
+	while (s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		putchar(*s);
+		s--;
+	}
+	putchar('\n');
+}
