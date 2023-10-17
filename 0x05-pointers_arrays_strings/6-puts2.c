3f435ffb82d74cd4b9f11b609269283beb575ff3
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-16 18:55:36 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - function prints one character of two
+ * starting with the first one
+ * @str: string
+ * Return: print
+ */
+
+void puts(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi -1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			putchar(str[o]);
+		}
+	}
+	putchar('\n');
+}
