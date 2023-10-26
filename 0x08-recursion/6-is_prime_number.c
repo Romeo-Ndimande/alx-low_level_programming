db69cec347bba81608d90395635f1f2d4173a129
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-26 06:25:23 -0700

    0x08 recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,19 +5,19 @@
  * @n: number to evaluate
  * Return: 1 if n is a prime number, 0 if it is not
  */
-int is_prime_number(int n)
+int _prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
 	return (_prime(n, n - 1));	
 }
 /**
- * is _prime_number - determines if number is a prime number or not
+ * _prime_number - determines if number is a prime number or not
  * @n: number to evaluate
  * @i: the iterator
  * Return: 1 if n is prime, else 0
  */
-int is_prime_number(int n, int i)
+int _prime_number(int n, int i)
 {
 	if (i == 1)
 		return (1);
