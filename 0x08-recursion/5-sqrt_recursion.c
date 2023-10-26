2154b96c03b58b4ee5580199fa3cf92f8381bbec
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-10-26 05:32:38 -0700

    sqrt recurssion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (_sqrt_recursion(n, 0));
 }
 /**
  * _sqrt_recursion - recurses to find natural square root of a number
