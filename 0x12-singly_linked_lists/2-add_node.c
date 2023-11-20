b1ec5f8864effb8dc3d29e947902d263ba3abbe9
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-11-20 05:10:23 -0800

    task 2 with non void function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,12 +22,12 @@ list_t *add_node(list_t **head, const char *str)
 		new_node->next = NULL;
 	else
 	{
-		new_node->next = *head;
+			new_node->next = *head;
 		new_node->str = strdup(str);
 		new_node->len = len(str);
 		*head = new_node;
 	}
-	return (new_node);
+	return (*head);
 }
 
 list_t *add_node(list_t **head, const char *str);
