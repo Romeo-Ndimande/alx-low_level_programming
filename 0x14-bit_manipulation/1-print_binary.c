04060a869b85fbf07fe3c298951452f8c2940d3a
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-11-23 13:37:11 -0800

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
  *
  * Return: value of (base ^ power)
  */
-unsigned long int _pow(unsigned int base, unsigned int power);
+unsigned long int _pow(unsigned int base, unsigned int power)
 {
 	unsigned long int num;
 	unsigned int a;
@@ -24,7 +24,7 @@ unsigned long int _pow(unsigned int base, unsigned int power);
  *
  * Return: void
  */
-void print_binary(unsigned long int n);
+void print_binary(unsigned long int n)
 {
 	unsigned long int divisor, check;
 	char flag;
