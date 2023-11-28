e3490090840b6cda5f54a2cb98852a58b75fb2e6
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-11-28 06:47:59 -0800

    under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - function creates a file
+ * @filename: file name
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fildes;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+	fildes = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	if (fildes == -1)
+		return (-1);
+	if (!text_content)
+		text_content = " ";
+	for (nletters = 0; text_content[nletters]; nletters++);
+	rwr = write(fildes, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fildes);
+	return (1);
+}
