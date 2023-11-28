3a5966ddad274941f281d58bd433a8f5cffd2d26
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-11-28 07:04:30 -0800

    under the snow 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = " ";
 	for
-	(nletters = 0, text_content[nletters], nletters++);
+	(nletters = 0; text_content[nletters]; nletters++);
 	rwr = write(fildes, text_content, nletters);
 
 	if (rwr == -1)
