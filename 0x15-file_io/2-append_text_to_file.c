249fc3ac7791daeeef875f2c19046422e3f16e32
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-11-30 07:56:07 -0800

    speak gently

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,10 +18,13 @@
 	if (text_content)
 	{
 		for 
-			(nletters = 0; text_content[nletters]; nlettters++);
+			(nletters = 0; text_content[nletters]; nlettters++)
+				;
 			rwr = write(fildes; text_content; nletters);
 			if (rwr == -1)
 				return (-1);
 	}
 	close(fildes);
+
+	return (1);
 }
