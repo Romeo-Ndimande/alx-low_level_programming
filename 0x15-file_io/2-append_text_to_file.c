c3032a6ac47f2010bf531c0781db7c95f8d247ec
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-11-30 08:09:11 -0800

    speak gently, attempt 5

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,9 +2,10 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: filename
- * @text_context: added content
+ * @text_content: added content
  * Return: 1 if the file exists, -1 if does not
  */
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fildes;
 	int nletters;
@@ -17,12 +18,12 @@
 		return (-1);
 	if (text_content)
 	{
-		for 
-			(nletters = 0; text_content[nletters]; nlettters++)
+		for (nletters = 0; text_content[nletters]; nlettters++)
+			;
+		rwr = write(fildes, text_content, nletters)
 				;
-			rwr = write(fildes; text_content; nletters);
-			if (rwr == -1)
-				return (-1);
+		if (rwr == -1)
+			return (-1);
 	}
 	close(fildes);
 
