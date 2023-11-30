ef989e9c7054995030dc22e735120b66ae41adf2
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-11-30 07:44:38 -0800

    under the sun 5

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		text_content = " ";
-	for
-	{
-		(nletters = 0; text_content[nletters]; nletters++);
-	}
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
 	rwr = write(fildes, text_content, nletters);
 
 	if (rwr == -1)
