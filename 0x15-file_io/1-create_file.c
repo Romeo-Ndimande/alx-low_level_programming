349f3d21487747df3a2aa7fe972f43264ef74520
Romeo-Ndimande <fortune.rndimande@gmail.com>
2023-11-30 07:23:52 -0800

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fildes = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	fildes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fildes == -1)
 		return (-1);
 	if (!text_content)
