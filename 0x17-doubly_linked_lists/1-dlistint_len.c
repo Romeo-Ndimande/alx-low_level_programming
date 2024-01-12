09a42e42e8c1e93490c4243aef66275a4456d224
Romeo-Ndimande <fortune.rndimande@gmail.com>
2024-01-12 00:40:56 -0800

    list length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns nu,ber of elements in a linked dlistint_t
- * @h: header
- * Return: number of elements
+ * dlistint_len - Return list len
+ * @h: struct list
+ * Return: list_len
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
-	int count = 0;
+	int i = 0;
 
-	for (count = 0; h; i++)
+	for (i = 0; h; i++)
 	{
 		len += 1;
 		h = h->next;
